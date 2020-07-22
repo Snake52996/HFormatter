4ec7d3ea4239255d4b30ad76cfde3c6b966dbe35
Snake52996 <33573135+Snake52996@users.noreply.github.com>
2020-07-22 19:45:02 +0800

    sort 3rdparty

---
+++ 3rd-lib/sqlite3/build/CMakeFiles/sqlite3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlite3.dir/sqlite3.c.obj"
+  "libsqlite3.dll"
+  "libsqlite3.dll.a"
+  "libsqlite3.dll.manifest"
+  "libsqlite3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sqlite3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
