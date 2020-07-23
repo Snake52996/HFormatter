fcd772277bd46898143a4d9bc99adefd1fa81e73
Snake52996 <33573135+Snake52996@users.noreply.github.com>
2020-07-23 21:24:28 +0800

    add syntactic sugar to Logger

--- source/logger.cpp
+++ source/logger.cpp
@@ -1,6 +1,5 @@
 #include<logger.hpp>
-#include<stdexcept>
 using std::shared_ptr;
 using HFormatter::NLogger::Logger;
 Logger::Logger():logger_(nullptr){};
-shared_ptr<spdlog::logger>& Logger::operator()(){return logger_;}
+shared_ptr<spdlog::logger>& Logger::operator->(){return logger_;}
